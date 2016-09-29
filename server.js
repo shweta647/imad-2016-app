92a6bd8fec397aac26dbc0ec7ec29025c3fe2cf5
shweta647 <shweta.rt.sharma@gmail.com>
2016-09-29 17:35:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,18 +9,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', fuction (req,res) {
-   res.send('Artcle one requested and will be serve here'); 
-});
-
-app.get('/article-two', fuction (req,res) {
-   res.send('Artcle one requested and will be serve here'); 
-});
-
-app.get('/article-three', fuction (req,res) {
-   res.send('Artcle one requested and will be serve here'); 
-});
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
