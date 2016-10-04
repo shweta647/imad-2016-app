73f61ded72e290fc5272cb448f07ca5bc98a4668
shweta647 <shweta.rt.sharma@gmail.com>
2016-10-04 19:33:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -85,7 +85,16 @@ var counter=0;
 app.get('/counter',function(req,res) {
     counter=counter+1;
     res.send(counter.toString());
-})
+});
+
+var names = [];
+app.get('/submit-name', function(req,res) {
+    var name = req.query.name;
+    
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+
 
 app.get('/:articleName', function (req,res) {
     var articleName = req.params.articleName;
