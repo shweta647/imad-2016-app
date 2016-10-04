90dcff409f86416c540375657bcdd47d109f639e
shweta647 <shweta.rt.sharma@gmail.com>
2016-10-04 13:12:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var articleOne={
 
 function createTemplate (data) {
     var title = data.title;
-    var data = data.date;
+    var date = data.date;
     var heading = data.heading;
     var content=data.content;
     
