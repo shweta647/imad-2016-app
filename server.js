66825d2cd25ff6e4ce98c656b56815030b5e3570
shweta647 <shweta.rt.sharma@gmail.com>
2016-10-04 14:35:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ function createTemplate (data) {
 }
     
 app.get('/', function (req, res) {
-  res.sendFile(path.join(_dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/:articleName', function (req,res) {
