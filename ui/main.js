6948cafef346f9a945a127339f84dd6a5605bdd6
shweta647 <shweta.rt.sharma@gmail.com>
2016-10-04 15:03:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ element.innerHTML='New Value';
 
 //move the image
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function movRight () {
+    marginLeft=marginLeft+10;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function () {
+    var interval = setInterval(moveRight,100);
     img.style.marginLeft = '100px';
 };
