eeccb85c32498dcc17f0e129ba64556c981e8760
shweta647 <shweta.rt.sharma@gmail.com>
2016-10-04 18:55:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var name = [];
+var names = [];
 app.get('/submit-name/:name', function (req, res) {
     //Get the name from the request
     var name= req.params.name;
