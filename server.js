0d4a1b410883dd155dbf2eeb20425dc9cec3ca6e
shweta647 <shweta.rt.sharma@gmail.com>
2016-10-04 13:35:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,12 +80,12 @@ function createTemplate (data) {
 }
     
 app.get('/', function (req, res) {
-  res.send(createTemplate(articleOne));
+  res.sendFile(path.join(_dirname, 'ui', 'index.html'));
 });
 
 app.get('/:articleName', function (req,res) {
     var articleName = req.params.articleName;
-    res.send(createTemplate(articles[article-Name]));
+    res.send(createTemplate(articles[articleName]));
 });
 
 
