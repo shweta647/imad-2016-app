a34d252969314b6fdfb916d897e46fe618151c45
shweta647 <shweta.rt.sharma@gmail.com>
2016-10-04 18:47:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,3 +14,19 @@ img.onclick = function () {
     var interval = setInterval(moveRight,50);
     img.style.marginLeft = '100px';
 };
+
+//submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function () {
+    //make a req to server and send the name
+    //capture  list of names and render it as a list
+    var names = ['JAVA', 'Python', 'Ruby on Rails'];
+    var list = '';
+    for (var i=0; i<names.length; i++) {
+        list+= '<li>' + names[i]+ '</li>';
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML = list;
+};
