1614441a2fa35e50adf316bd6ca7379b2efd10e3
shweta647 <shweta.rt.sharma@gmail.com>
2016-10-20 18:58:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,3 +43,5 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 request.open('GET', 'http://shweta647.imad.hasura-app.io/submit-name?name=' +name,true);
 request.send(null);
+
+};
