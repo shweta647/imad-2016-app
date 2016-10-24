b8a1139ced84601c293dad530630746b5d36690d
shweta647 <shweta.rt.sharma@gmail.com>
2016-10-24 15:42:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -52,7 +52,7 @@ submit.onclick = function () {
                 for (var i=0; i<names.length; i++) {
                     list += '<li>' + names[i]+ '</li>';
                 }
-                var submit = document.getElementById('namelist');
+                var ul = document.getElementById('namelist');
                 ul.innerHTML = list;
             }
         }
