0c47fdbc17cc36df851a635cc4ac96e029e73289
shweta647 <shweta.rt.sharma@gmail.com>
2016-10-24 15:39:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -36,8 +36,6 @@ button.onclick=function () {
 };
 
 //submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     // make a request to the server and send the name
