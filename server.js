3f2d6e8d386a395944cf919d89949f5eed729709
shweta647 <shweta.rt.sharma@gmail.com>
2016-11-04 12:42:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ app.get('/submit-name', function (req, res) {
 app.get('/articles/:articleName', function (req,res) {
     
     //SELECT * FROM article WHERE title= 'article-one'
-    pool.query("SELECT * FROM article WHERE title='" +req.params.articleName+ "'", function (err, result) {
+    pool.query("SELECT * FROM article WHERE title=$1", [req.params.articleName], function (err, result) {
        if (err) {
            re.status(500).send(err.toString());
        } else {
