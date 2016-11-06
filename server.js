e2fe76e5b66fba933de97c2c3f05797bb9962eaf
shweta647 <shweta.rt.sharma@gmail.com>
2016-11-06 17:26:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,6 +108,10 @@ app.post('/login', function (req,res) {
               var hashedPassword = hash(password, salt); //Creating a hash based on the password submitted and the original salt
               if (hashedPassword === dbString) {
               res.send('credentials correct!');
+              
+              //Set a session
+              
+              
           } else {
               res.send(403).send('username/password is invalid');
           }
